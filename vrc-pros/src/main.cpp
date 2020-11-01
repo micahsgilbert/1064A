a8757975095a5937c667a1a0c97a261acf80edc3
micahsgilbert <35547303+micahsgilbert@users.noreply.github.com>
2020-11-01 15:35:08 -0600

    started work on drive programming

--- vrc-pros/src/main.cpp
+++ vrc-pros/src/main.cpp
@@ -1,5 +1,7 @@
 #include "main.h"
 
+// modes = "TANK", "ARCADE", "CUSTOM"
+char mode[] = "TANK";
 
 void disabled() {}
 
@@ -8,29 +10,26 @@ void competition_initialize() {}
 void autonomous() {}
 
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
+	pros::Controller controller(pros::E_CONTROLLER_MASTER);
 	pros::Motor left_drive(1);
   pros::Motor right_drive(1, true);
 
   while(true) {
-    left_drive.move(127);
-    right_drive.move(127);
+    // get controller values
 
-    pros::delay(2000);
+    int leftMotorSpeed;
+    int rightMotorSpeed;
 
-    left_drive.move(0);
-    right_drive.move(0);
+    if (mode == "TANK") {
+      leftMotorSpeed = controller.get_analog(ANALOG_LEFT_Y);
+      rightMotorSpeed = controller.get_analog(ANALOG_RIGHT_Y);
 
-    pros::delay(2000);
-
-    left_drive.move(-127);
-    right_drive.move(-127);
-
-    pros::delay(2000);
-
-    left_drive.move(0);
-    right_drive.move(0);
-
-    pros::delay(2000);
+    } else if (mode == "ARCADE") {
+      controller.set_text(0, 0, "ARCADE mode not implemented");
+    } else if (mode == "CUSTOM") {
+      controller.set_text(0, 0, "CUSTOM mode not implemented");
+    } else {
+      controller.set_text(0, 0, "Unknown mode");
+    }
   }
 }
