26c5053b7b41130f115aba1f7a67865b74b77dac
micahsgilbert <35547303+micahsgilbert@users.noreply.github.com>
2020-11-01 15:45:32 -0600

    fixed drivig

--- vrc-pros/src/main.cpp
+++ vrc-pros/src/main.cpp
@@ -1,7 +1,7 @@
 #include "main.h"
 
-// modes = "TANK", "ARCADE", "CUSTOM"
-char mode[] = "TANK";
+// modes = 0: "TANK", 1: "ARCADE", 2: "CUSTOM"
+int mode = 0;
 
 void disabled() {}
 
@@ -12,24 +12,36 @@ void autonomous() {}
 void opcontrol() {
 	pros::Controller controller(pros::E_CONTROLLER_MASTER);
 	pros::Motor left_drive(1);
-  pros::Motor right_drive(1, true);
+  pros::Motor right_drive(2, true);
 
   while(true) {
     // get controller values
 
-    int leftMotorSpeed;
-    int rightMotorSpeed;
+    int left_motor_speed = 0;
+    int right_motor_speed = 0;
 
-    if (mode == "TANK") {
-      leftMotorSpeed = controller.get_analog(ANALOG_LEFT_Y);
-      rightMotorSpeed = controller.get_analog(ANALOG_RIGHT_Y);
+    if (mode == 0) {
+      left_motor_speed = controller.get_analog(ANALOG_LEFT_Y);
+      right_motor_speed = controller.get_analog(ANALOG_RIGHT_Y);
 
-    } else if (mode == "ARCADE") {
+    } else if (mode == 1) {
       controller.set_text(0, 0, "ARCADE mode not implemented");
-    } else if (mode == "CUSTOM") {
+      while(true) {
+        pros::delay(100);
+      }
+    } else if (mode == 2) {
       controller.set_text(0, 0, "CUSTOM mode not implemented");
+      while(true) {
+        pros::delay(100);
+      }
     } else {
       controller.set_text(0, 0, "Unknown mode");
+      while(true) {
+        pros::delay(100);
+      }
     }
+
+    left_drive.move(left_motor_speed);
+    right_drive.move(right_motor_speed);
   }
 }
