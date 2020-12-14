28f4b916ebae16b7efa6c63e3268a58469404921
Micah Gilbert <35547303+someRandomGuy750@users.noreply.github.com>
2020-12-13 22:17:58 -0600

    fix mobile

--- site/index.js
+++ site/index.js
@@ -2,5 +2,5 @@ const resizeCoverOverlay = () => {
   document.getElementById("gradient-cover").style.height = Math.min(document.getElementById("cover-image").height, window.innerHeight) + "px"
 }
 
-
+window.addEventListener("load", resizeCoverOverlay)
 window.addEventListener('resize', resizeCoverOverlay)
