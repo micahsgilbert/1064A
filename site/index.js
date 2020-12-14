da79c19a1fb3dadbbacdcd53852ffb04d8c4cea4
Micah Gilbert <35547303+someRandomGuy750@users.noreply.github.com>
2020-12-14 11:41:30 -0600

    fixed image loading yet again

--- site/index.js
+++ site/index.js
@@ -1,6 +1,6 @@
 const resizeCoverOverlay = () => {
-  document.getElementById("gradient-cover").style.height = Math.min(document.getElementById("cover-image").height, window.innerHeight) + "px"
+  document.getElementById("gradient-cover").style.height = Math.min(window.innerHeight, 0.6434 * window.innerWidth) + "px"
 }
 
 window.addEventListener("load", resizeCoverOverlay)
-window.addEventListener('resize', resizeCoverOverlay)
+window.addEventListener("resize", resizeCoverOverlay)
