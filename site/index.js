6fee4a86287a7427849c8c8ac1427444b7e39ed2
Micah Gilbert <35547303+someRandomGuy750@users.noreply.github.com>
2020-12-13 19:55:31 -0600

    improve image loading

--- site/index.js
+++ site/index.js
@@ -1,5 +1,5 @@
 const resizeCoverOverlay = () => {
-  document.getElementById("gradient-cover").style.height = Math.min(document.getElementById("cover").height, window.innerHeight) + "px"
+  document.getElementById("gradient-cover").style.height = Math.min(document.getElementById("cover-image").height, window.innerHeight) + "px"
 }
 
 window.addEventListener("load", resizeCoverOverlay)
