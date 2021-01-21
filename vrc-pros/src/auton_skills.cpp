67602da5fa85c630a3bcc6bdd2f66b0cb7b9a4c7
micahsgilbert <35547303+micahsgilbert@users.noreply.github.com>
2021-01-21 15:49:23 -0600

    start work on auton_skills

--- vrc-pros/src/auton_skills.cpp
+++ vrc-pros/src/auton_skills.cpp
@@ -1,6 +1,12 @@
 #include "auton_skills.h"
 #include "auton.h"
+#include "motors.h"
 
 void auton_skills() {
+  motors::left_lift.move(127);
+  motors::right_lift.move(127);
+  pros::delay(5000);
+  motors::left_lift.move(0);
+  motors::right_lift.move(0);
   return;
 }
--- vrc-pros/src/main.cpp
+++ vrc-pros/src/main.cpp
@@ -2,6 +2,7 @@
 #include "config.h"
 #include "motors.h"
 #include "auton.h"
+#include "auton_skills.h"
 
 /*
 CONTROLS
