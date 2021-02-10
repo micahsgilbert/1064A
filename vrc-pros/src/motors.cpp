53c8718ff66ae822237c870a3b4702df709f7766
Micah Gilbert <35547303+micahsgilbert@users.noreply.github.com>
2021-02-10 15:49:18 -0600

    update motor ports

--- vrc-pros/src/motors.cpp
+++ vrc-pros/src/motors.cpp
@@ -5,9 +5,9 @@ namespace motors {
   pros::Motor left_drive(1);
   pros::Motor right_drive(2, true);
 
-  pros::Motor left_lift(3, true);
-  pros::Motor right_lift(4);
+  pros::Motor left_lift(9, true);
+  pros::Motor right_lift(10);
 
-  pros::Motor left_intake(5, pros::E_MOTOR_GEARSET_36);
-  pros::Motor right_intake(6, pros::E_MOTOR_GEARSET_36, true);
+  pros::Motor left_intake(19, pros::E_MOTOR_GEARSET_36);
+  pros::Motor right_intake(20, pros::E_MOTOR_GEARSET_36, true);
 }
